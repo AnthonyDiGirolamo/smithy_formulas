f1dc11d9852cabdc692eacbad50dabc14aacae65
AnthonyDiGirolamo <anthony.digirolamo@gmail.com>
2012-12-18 14:18:53 -0500

    moved formulas outside lib

---
+++ curl_formula.rb
@@ -0,0 +1,11 @@
+class Curl_formula < Formula
+  homepage 'http://curl.haxx.se/'
+  url      'http://curl.haxx.se/download/curl-7.28.1.tar.bz2'
+  md5      '26eb081c999b0e203770869427e9a93d'
+
+  def install
+    system "./configure --prefix=#{prefix}"
+    system "make"
+    system "make install"
+  end
+end
