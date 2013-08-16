eb82101f66314e48cc5fa9d91ed97569106072b1
AnthonyDiGirolamo <anthony.digirolamo@gmail.com>
2013-08-16 17:26:11 +0000

    added python_numexpr_formula

---
+++ python_numexpr_formula.rb
@@ -0,0 +1,94 @@
+class PythonNumexprFormula < Formula
+  homepage "http://code.google.com/p/numexpr/"
+  url "http://numexpr.googlecode.com/files/numexpr-2.1.tar.gz"
+  sha1 "4cce65ba0581bed2a4b3a43f7a102c359404bc44"
+
+  depends_on do
+    packages = [ ]
+    case build_name
+    when /python3.3/
+      packages << "python/3.3.0"
+    when /python2.7/
+      packages << "python/2.7.3"
+    when /python2.6/
+    end
+    packages
+  end
+
+  module_commands do
+    m = [ "unload PrgEnv-gnu PrgEnv-pgi PrgEnv-cray PrgEnv-intel" ]
+    case build_name
+    when /gnu/
+      m << "load PrgEnv-gnu"
+    when /pgi/
+      m << "load PrgEnv-pgi"
+    when /intel/
+      m << "load PrgEnv-intel"
+    when /cray/
+      m << "load PrgEnv-cray"
+    end
+    m << "unload python"
+
+    case build_name
+    when /python3.3/
+      m << "load python/3.3.0"
+    when /python2.7/
+      m << "load python/2.7.3"
+    end
+    m
+  end
+
+  def install
+    module_list
+
+    python_binary = "python"
+    libdirs = []
+    case build_name
+    when /python3.3/
+      python_binary = "python3.3"
+      libdirs << "#{prefix}/lib/python3.3/site-packages"
+    when /python2.7/
+      libdirs << "#{prefix}/lib/python2.7/site-packages"
+    when /python2.6/
+      libdirs << "#{prefix}/lib64/python2.6/site-packages"
+    end
+    FileUtils.mkdir_p libdirs.first
+
+    python_start_command = "PYTHONPATH=$PYTHONPATH:#{libdirs.join(":")} #{python_binary} "
+
+    system "#{python_start_command} setup.py build"
+    system "#{python_start_command} setup.py install --prefix=#{prefix} --compile"
+  end
+
+  modulefile <<-MODULEFILE.strip_heredoc
+    #%Module
+    proc ModulesHelp { } {
+       puts stderr "<%= @package.name %> <%= @package.version %>"
+       puts stderr ""
+    }
+    # One line description
+    module-whatis "<%= @package.name %> <%= @package.version %>"
+
+    module load python_numpy
+    prereq python_numpy
+
+    if [ is-loaded python/3.3.0 ] {
+      set BUILD python3.3.0
+      set LIBDIR python3.3
+    } elseif { [ is-loaded python/2.7.3 ] || [ is-loaded python/2.7.2 ] } {
+      set BUILD python2.7.3
+      set LIBDIR python2.7
+    } else {
+      set BUILD python2.6.8
+      set LIBDIR python2.6
+    }
+    set PREFIX <%= @package.version_directory %>/$BUILD
+
+    prepend-path PATH            $PREFIX/bin
+    prepend-path LD_LIBRARY_PATH $PREFIX/lib
+    prepend-path LD_LIBRARY_PATH $PREFIX/lib64
+    prepend-path MANPATH         $PREFIX/share/man
+    prepend-path PYTHONPATH      $PREFIX/lib/$LIBDIR/site-packages
+    prepend-path PYTHONPATH      $PREFIX/lib64/$LIBDIR/site-packages
+  MODULEFILE
+end
