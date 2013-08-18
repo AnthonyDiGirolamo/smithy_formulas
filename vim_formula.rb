3117f5289fa79ed37592f108f4cb8ed960b87f24
AnthonyDiGirolamo <anthony.digirolamo@gmail.com>
2013-08-18 12:25:21 -0400

    update vim version

--- vim_formula.rb
+++ vim_formula.rb
@@ -1,8 +1,9 @@
 class VimFormula < Formula
   homepage "http://www.vim.org"
-  url      "http://ftp.vim.org/pub/vim/unix/vim-7.3.tar.bz2"
+  url "http://ftp.vim.org/pub/vim/unix/vim-7.4.tar.bz2"
+  sha1 '601abf7cc2b5ab186f40d8790e542f86afca86b7'
 
-  version "7.3"
+  module_commands ["purge"]
 
   def install
     module_list
@@ -10,4 +11,18 @@ class VimFormula < Formula
     system "make"
     system "make install"
   end
+
+  modulefile <<-MODULEFILE.strip_heredoc
+    #%Module
+    proc ModulesHelp { } {
+       puts stderr "<%= @package.name %> <%= @package.version %>"
+       puts stderr ""
+    }
+    module-whatis "<%= @package.name %> <%= @package.version %>"
+
+    set PREFIX <%= @package.prefix %>
+
+    prepend-path PATH      $PREFIX/bin
+    prepend-path MANPATH   $PREFIX/share/man
+  MODULEFILE
 end
