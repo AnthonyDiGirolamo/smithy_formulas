d4fb63279ac26dcf04a80909267a9746601ee38c
AnthonyDiGirolamo <anthony.digirolamo@gmail.com>
2013-08-19 12:39:15 -0400

    added modulefile to the subversion_formula

--- subversion_formula.rb
+++ subversion_formula.rb
@@ -24,4 +24,19 @@ class SubversionFormula < Formula
     system "make"
     system "make install"
   end
+
+  modulefile <<-MODULEFILE.strip_heredoc
+    #%Module
+    proc ModulesHelp { } {
+       puts stderr "<%= @package.name %> <%= @package.version %>"
+       puts stderr ""
+    }
+    module-whatis "<%= @package.name %> <%= @package.version %>"
+
+    set PREFIX <%= @package.prefix %>
+
+    prepend-path PATH            $PREFIX/bin
+    prepend-path LD_LIBRARY_PATH $PREFIX/lib
+    prepend-path MANPATH         $PREFIX/share/man
+  MODULEFILE
 end
