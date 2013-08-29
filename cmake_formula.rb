454c3eef750820d6425396e54e8c4d7af7d1cb39
AnthonyDiGirolamo <anthony.digirolamo@gmail.com>
2013-08-29 10:49:19 -0400

    forgot a module purge for cmake

--- cmake_formula.rb
+++ cmake_formula.rb
@@ -2,6 +2,8 @@ class CmakeFormula < Formula
   homepage "http://www.cmake.org/"
   url      "http://www.cmake.org/files/v2.8/cmake-2.8.11.2.tar.gz"
 
+  module_commands [ "purge" ]
+
   def install
     module_list
     system "./bootstrap --prefix=#{prefix} --no-qt-gui"
