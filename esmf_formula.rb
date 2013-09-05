7dfce62a6ad8973e37b82712b0fbaa79670d4772
AnthonyDiGirolamo <anthony.digirolamo@gmail.com>
2013-09-05 18:04:03 +0000

    updated esmf_formula

--- esmf_formula.rb
+++ esmf_formula.rb
@@ -32,13 +32,17 @@ class EsmfFormula < Formula
     module_list
 
     ENV["ESMF_BOPT"] = "O"
-    ENV["ESMF_OS"] = "Unicos"
-    ENV["ESMF_DIR"] = "#{prefix}/source"
+    ENV["ESMF_OS"]   = "Unicos"
+    ENV["ESMF_DIR"]  = "#{prefix}/source"
     ENV["ESMF_COMM"] = "mpi"
 
+    ENV["ESMF_INSTALL_BINDIR"]    = "#{prefix}/bin"
+    ENV["ESMF_INSTALL_LIBDIR"]    = "#{prefix}/lib"
+    ENV["ESMF_INSTALL_HEADERDIR"] = "#{prefix}/include"
+
+    ENV["ESMF_COMPILER"] = "default"
     ENV["ESMF_COMPILER"] = "pgi"   if build_name.include?("pgi")
     ENV["ESMF_COMPILER"] = "intel" if build_name.include?("intel")
-    ENV["ESMF_COMPILER"] = "gnu"   if build_name.include?("gnu")
 
     ENV["ESMF_MPIRUN"] = "mpirun.unicos.batch"
     ENV["ESMF_MPIBATCHOPTIONS"] = "-A STF007 -q debug"
@@ -65,26 +69,11 @@ class EsmfFormula < Formula
 
   modulefile <<-MODULEFILE.strip_heredoc
     #%Module
-    # ESMF, modeling framework
-
-    global app_name_g
-    set app_name_g ESMF
-    global app_version_g
-    set app_version_g 5.2.0rp2
-
     proc ModulesHelp { } {
-    #   puts stderr "Sets up environment to use ${app_name_g} version
-    #   ${app_version_g} built with -g"
-        puts stderr "Sets up environment to use ESMF version 5.2.0-p1 built with -O"
+       puts stderr "<%= @package.name %> <%= @package.version %>"
+       puts stderr ""
     }
-
-    module-whatis "Sets up environment to use $app_name_g  version $app_version_g built with -O"
-
-    set machine xk6
-    set version $app_version_g
-    set esmfbase /sw/$machine/esmf/$version
-
-    set build_opt O
+    module-whatis "<%= @package.name %> <%= @package.version %>"
 
     <% if @builds.size > 1 %>
     <%= module_build_list @package, @builds %>
@@ -94,10 +83,8 @@ class EsmfFormula < Formula
     set PREFIX <%= @package.prefix %>
     <% end %>
 
-    set esmfpath $esmfbase
-
-    setenv ESMF_BINDIR "$esmfpath/$esmfbuild/bin"
-    setenv ESMF_LIBDIR "$esmfpath/$esmfbuild/lib"
-    setenv ESMFMKFILE "$esmfpath/$esmfbuild/lib/esmf.mk"
+    setenv ESMF_BINDIR $PREFIX/bin
+    setenv ESMF_LIBDIR $PREFIX/lib
+    setenv ESMFMKFILE  $PREFIX/lib/esmf.mk
   MODULEFILE
 end
