2e4635b58c861194d65d1d3de82d635e1c290110
AnthonyDiGirolamo <anthony.digirolamo@gmail.com>
2013-09-12 10:28:27 -0400

    added gsl modulefile

--- gsl_formula.rb
+++ gsl_formula.rb
@@ -9,4 +9,30 @@ class GslFormula < Formula
     system "make"
     system "make install"
   end
+
+  modulefile <<-EOF.strip_heredoc
+    #%Module
+    proc ModulesHelp { } {
+       puts stderr "<%= @package.name %> <%= @package.version %>"
+       puts stderr ""
+    }
+    module-whatis "<%= @package.name %> <%= @package.version %>"
+
+    <% if @builds.size > 1 %>
+    <%= module_build_list @package, @builds %>
+
+    set PREFIX <%= @package.version_directory %>/$BUILD
+    <% else %>
+    set PREFIX <%= @package.prefix %>
+    <% end %>
+
+    setenv       GSL_DIR            $PREFIX
+    set          GSL_INCLUDE_PATH   "-I$PREFIX/include"
+    set          GSL_LD_OPTS        "-L$PREFIX/lib -lgsl -lgslcblas"
+    setenv       GSL_LIB            "$GSL_INCLUDE_PATH $GSL_LD_OPTS"
+
+    prepend-path PE_PRODUCT_LIST    "GSL"
+    setenv       GSL_INCLUDE_OPTS   "-I$PREFIX/include"
+    setenv       GSL_POST_LINK_OPTS "-L$PREFIX/lib -lgsl -lgslcblas"
+  EOF
 end
