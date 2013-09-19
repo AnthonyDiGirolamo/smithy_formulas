7fb83b578cc01c88d2ddd387c79d907c45c9a97a
Stuart Campbell <campbellsi@ornl.gov>
2013-09-19 13:50:58 -0400

    Added modulefile to formula.

--- cmake_formula.rb
+++ cmake_formula.rb
@@ -10,4 +10,18 @@ class CmakeFormula < Formula
     system "make all"
     system "make install"
   end
+
+  modulefile <<-MODULEFILE.strip_heredoc
+    #%Module
+    proc ModulesHelp { } {
+      puts stderr "<%= @package.name %> <%= @package.version %>"
+      puts stderr ""
+    }
+    module-whatis "<%= @package.name %> <%= @package.version %>"
+
+    set PREFIX <%= @package.prefix %>
+    
+    prepend-path PATH      $PREFIX/bin
+    prepend-path MANPATH   $PREFIX/share/man
+  MODULEFILE
 end
