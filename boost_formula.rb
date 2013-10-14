26be738fc2b0e197900f2451b8b95cbc7516f4e9
AnthonyDiGirolamo <anthony.digirolamo@gmail.com>
2013-10-14 18:12:23 -0400

    updated boost for intel and pgi compilers

--- boost_formula.rb
+++ boost_formula.rb
@@ -1,7 +1,7 @@
 class BoostFormula < Formula
   homepage "http://www.boost.org/"
-  url      "http://downloads.sourceforge.net/project/boost/boost/1.53.0/boost_1_53_0.tar.bz2"
-  sha256   "f88a041b01882b0c9c5c05b39603ec8383fb881f772f6f9e6e6fd0e0cddb9196"
+  url      "http://downloads.sourceforge.net/project/boost/boost/1.54.0/boost_1_54_0.tar.bz2"
+  sha256   "047e927de336af106a24bceba30069980c191529fd76b8dff8eb9a328b48ae1d"
 
   depends_on [ "bzip2" ]
 
@@ -12,6 +12,8 @@ class BoostFormula < Formula
       m << "load PrgEnv-gnu"
     when /pgi/
       m << "load PrgEnv-pgi"
+    when /intel/
+      m << "load PrgEnv-intel"
     end
   end
 
@@ -40,66 +42,67 @@ class BoostFormula < Formula
           ;
         EOF
       end
+
     when /pgi/
       toolset="pgi"
 
-      # Boost Ticket: https://svn.boost.org/trac/boost/ticket/8333
-      patch <<-EOF.strip_heredoc
-        diff --git a/boost/math/special_functions/sinc.hpp b/boost/math/special_functions/sinc.hpp
-        index ffb19d8..8d2a8a6 100644
-        --- a/boost/math/special_functions/sinc.hpp
-        +++ b/boost/math/special_functions/sinc.hpp
-        @@ -52,16 +52,7 @@ namespace boost
-                 template<typename T>
-                 inline T    sinc_pi_imp(const T x)
-                 {
-        -#if defined(BOOST_NO_STDC_NAMESPACE) && !defined(__SUNPRO_CC)
-        -            using    ::abs;
-        -            using    ::sin;
-        -            using    ::sqrt;
-        -#else    /* BOOST_NO_STDC_NAMESPACE */
-        -            using    ::std::abs;
-        -            using    ::std::sin;
-        -            using    ::std::sqrt;
-        -#endif    /* BOOST_NO_STDC_NAMESPACE */
-        -
-        +BOOST_MATH_STD_USING
-                     // Note: this code is *not* thread safe!
-                     static T const    taylor_0_bound = tools::epsilon<T>();
-                     static T const    taylor_2_bound = sqrt(taylor_0_bound);
-      EOF
+      # # Boost Ticket: https://svn.boost.org/trac/boost/ticket/8333
+      # patch <<-EOF.strip_heredoc
+      #   diff --git a/boost/math/special_functions/sinc.hpp b/boost/math/special_functions/sinc.hpp
+      #   index ffb19d8..8d2a8a6 100644
+      #   --- a/boost/math/special_functions/sinc.hpp
+      #   +++ b/boost/math/special_functions/sinc.hpp
+      #   @@ -52,16 +52,7 @@ namespace boost
+      #            template<typename T>
+      #            inline T    sinc_pi_imp(const T x)
+      #            {
+      #   -#if defined(BOOST_NO_STDC_NAMESPACE) && !defined(__SUNPRO_CC)
+      #   -            using    ::abs;
+      #   -            using    ::sin;
+      #   -            using    ::sqrt;
+      #   -#else    /* BOOST_NO_STDC_NAMESPACE */
+      #   -            using    ::std::abs;
+      #   -            using    ::std::sin;
+      #   -            using    ::std::sqrt;
+      #   -#endif    /* BOOST_NO_STDC_NAMESPACE */
+      #   -
+      #   +BOOST_MATH_STD_USING
+      #                // Note: this code is *not* thread safe!
+      #                static T const    taylor_0_bound = tools::epsilon<T>();
+      #                static T const    taylor_2_bound = sqrt(taylor_0_bound);
+      # EOF
 
-      # Boost Ticket: https://svn.boost.org/trac/boost/ticket/8394
-      patch <<-EOF.strip_heredoc
-        diff -ur boost_1_53_0/libs/mpi/src/python/py_environment.cpp boost_1_53_0.2/libs/mpi/src/python/py_environment.cpp
-        --- boost_1_53_0/libs/mpi/src/python/py_environment.cpp 2007-11-25 12:38:02.000000000 -0600
-        +++ boost_1_53_0.2/libs/mpi/src/python/py_environment.cpp       2013-04-04 10:16:05.000000000 -0500
-        @@ -31,7 +31,7 @@
-          */
-         static environment* env;
+      # # Boost Ticket: https://svn.boost.org/trac/boost/ticket/8394
+      # patch <<-EOF.strip_heredoc
+      #   diff -ur boost_1_53_0/libs/mpi/src/python/py_environment.cpp boost_1_53_0.2/libs/mpi/src/python/py_environment.cpp
+      #   --- boost_1_53_0/libs/mpi/src/python/py_environment.cpp 2007-11-25 12:38:02.000000000 -0600
+      #   +++ boost_1_53_0.2/libs/mpi/src/python/py_environment.cpp       2013-04-04 10:16:05.000000000 -0500
+      #   @@ -31,7 +31,7 @@
+      #     */
+      #    static environment* env;
 
-        -bool mpi_init(list python_argv, bool abort_on_exception)
-        +bool mpi_init(boost::python::list python_argv, bool abort_on_exception)
-         {
-           // If MPI is already initialized, do nothing.
-           if (environment::initialized())
-        @@ -79,7 +79,7 @@
-           if (!environment::initialized()) {
-             // MPI_Init from sys.argv
-             object sys = object(handle<>(PyImport_ImportModule("sys")));
-        -    mpi_init(extract<list>(sys.attr("argv")), true);
-        +    mpi_init(extract<boost::python::list>(sys.attr("argv")), true);
+      #   -bool mpi_init(list python_argv, bool abort_on_exception)
+      #   +bool mpi_init(boost::python::list python_argv, bool abort_on_exception)
+      #    {
+      #      // If MPI is already initialized, do nothing.
+      #      if (environment::initialized())
+      #   @@ -79,7 +79,7 @@
+      #      if (!environment::initialized()) {
+      #        // MPI_Init from sys.argv
+      #        object sys = object(handle<>(PyImport_ImportModule("sys")));
+      #   -    mpi_init(extract<list>(sys.attr("argv")), true);
+      #   +    mpi_init(extract<boost::python::list>(sys.attr("argv")), true);
 
-             // Setup MPI_Finalize call when the program exits
-             object atexit = object(handle<>(PyImport_ImportModule("atexit")));
-      EOF
+      #        // Setup MPI_Finalize call when the program exits
+      #        object atexit = object(handle<>(PyImport_ImportModule("atexit")));
+      # EOF
 
       File.open("tools/build/v2/site-config.jam", "w+") do |f|
         f.write <<-EOF.strip_heredoc
           import os ;
           local CRAY_MPICH2_DIR = [ os.environ CRAY_MPICH2_DIR ] ;
           using pgi
-            : 13.3.0.xk7
+            : 13.7.0.xk7
             : pgCC
             : <compileflags>-I#{bzip2.prefix}/include
               <compileflags>-I$(CRAY_MPICH2_DIR)/include
@@ -107,8 +110,30 @@ class BoostFormula < Formula
               <compileflags>-mp
           ;
           using mpi
-            : pgCC
-            : <find-shared-library>mpich_pgi
+            : CC
+            : <find-shared-library>mpichcxx_pgi
+            : aprun -n
+          ;
+        EOF
+      end
+
+    when /intel/
+      toolset="intel-linux"
+
+      File.open("tools/build/v2/user-config.jam", "w+") do |f|
+        f.write <<-EOF.strip_heredoc
+          import os ;
+          local CRAY_MPICH2_DIR = [ os.environ CRAY_MPICH2_DIR ] ;
+          using intel-linux
+            : 13.1.3.192
+            : icpc
+            : <compileflags>-I#{bzip2.prefix}/include
+              <compileflags>-I$(CRAY_MPICH2_DIR)/include
+              <linkflags>-L$(CRAY_MPICH2_DIR)/lib
+          ;
+          using mpi
+            : CC
+            : <find-shared-library>mpichcxx_intel
             : aprun -n
           ;
         EOF
@@ -116,7 +141,18 @@ class BoostFormula < Formula
     end
 
     system "./bootstrap.sh --with-toolset=#{toolset} --prefix=#{prefix}"
+
+    if build_name.include?("intel")
+      # remove redundant using intel-linux definition that bootstrap.sh spits
+      # out in the project-config.jam
+      contents = File.read("project-config.jam").gsub(/if ! intel-linux in \[ feature.values <toolset> \].*{.*using intel-linux ;.*}/m, '')
+      File.open("project-config.jam", "w+") do |f|
+        f.write contents
+      end
+    end
+
     # system "./b2 toolset=#{toolset} link=static --clean"
+    # system "./b2 link=static --user-config=#{prefix}/source/tools/build/v2/user-config.jam --debug-configuration install"
     system "./b2 toolset=#{toolset} link=static --debug-configuration install"
   end
 
