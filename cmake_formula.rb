77a82eb1e5da4c456f5f8387901142011bfaef21
AnthonyDiGirolamo <anthony.digirolamo@gmail.com>
2013-10-15 12:58:42 -0400

    cmake using intel and cray wrappers doesn't like -rdynamic

--- cmake_formula.rb
+++ cmake_formula.rb
@@ -6,9 +6,28 @@ class CmakeFormula < Formula
 
   def install
     module_list
+
     system "./bootstrap --prefix=#{prefix} --no-qt-gui"
     system "make all"
     system "make install"
+
+    Dir.chdir prefix
+    patch <<-EOF.strip_heredoc
+      diff --git a/share/cmake-2.8/Modules/Platform/Linux-Intel.cmake.original b/share/cmake-2.8/Modules/Platform/Linux-Intel
+      index 2394f10..ad2c75a 100644
+      --- a/share/cmake-2.8/Modules/Platform/Linux-Intel.cmake.original
+      +++ b/share/cmake-2.8/Modules/Platform/Linux-Intel.cmake
+      @@ -38,7 +38,8 @@ macro(__linux_compiler_intel lang)
+
+         # We pass this for historical reasons.  Projects may have
+         # executables that use dlopen but do not set ENABLE_EXPORTS.
+      -  set(CMAKE_SHARED_LIBRARY_LINK_${lang}_FLAGS "-rdynamic")
+      +  # set(CMAKE_SHARED_LIBRARY_LINK_${lang}_FLAGS "-rdynamic")
+      +  set(CMAKE_SHARED_LIBRARY_LINK_${lang}_FLAGS "")
+
+         if(XIAR)
+           # INTERPROCEDURAL_OPTIMIZATION
+    EOF
   end
 
   modulefile <<-MODULEFILE.strip_heredoc
@@ -20,7 +39,7 @@ class CmakeFormula < Formula
     module-whatis "<%= @package.name %> <%= @package.version %>"
 
     set PREFIX <%= @package.prefix %>
-    
+
     prepend-path PATH      $PREFIX/bin
     prepend-path MANPATH   $PREFIX/share/man
   MODULEFILE
