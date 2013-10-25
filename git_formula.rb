ccf34dd9c448b6c3e55d559ab27d3ab205e92409
Robert D. French <frenchrd@ornl.gov>
2013-10-25 15:53:01 -0400

    Download and install Error.pm alongside git

--- git_formula.rb
+++ git_formula.rb
@@ -1,7 +1,7 @@
 class GitFormula < Formula
   homepage "https://git-core.googlecode.com/"
-  url "https://git-core.googlecode.com/files/git-1.8.3.4.tar.gz"
-  sha1 "fe633d02f7d964842d7ea804278b75120fc60c11"
+  url "https://git-core.googlecode.com/files/git-1.8.4.1.tar.gz"
+  sha1 "49004a8dfcbb7c0848147737d9877fd7313a42ec"
   depends_on "curl"
 
   module_commands [ "purge" ]
@@ -13,8 +13,14 @@ class GitFormula < Formula
     system "make install"
 
     system "mkdir -p #{prefix}/share/man"
-    system "curl -O https://git-core.googlecode.com/files/git-manpages-1.8.3.4.tar.gz"
-    system "cd #{prefix}/share/man && tar xf #{prefix}/source/git-manpages-1.8.3.4.tar.gz"
+    system "curl -O https://git-core.googlecode.com/files/git-manpages-1.8.4.1.tar.gz" 
+    system "cd #{prefix}/share/man && tar xf #{prefix}/source/git-manpages-1.8.4.1.tar.gz"
+
+    system "wget http://search.cpan.org/CPAN/authors/id/S/SH/SHLOMIF/Error-0.17021.tar.gz"
+    system "tar xf Error-0.17021.tar.gz"
+    Dir.chdir prefix + "/source/Error-0.17021"
+    system "perl Makefile.PL PREFIX=#{prefix}"
+    system "make install"
   end
 
   modulefile <<-MODULEFILE.strip_heredoc
@@ -28,7 +34,7 @@ class GitFormula < Formula
     set PREFIX <%= @package.prefix %>
 
     prepend-path PATH      $PREFIX/bin
-    prepend-path PERL5PATH $PREFIX/lib64/perl5/site_perl
+    prepend-path PERL5LIB  $PREFIX/lib64/perl5/site_perl
     prepend-path MANPATH   $PREFIX/share/man
     setenv       GITDIR    $PREFIX
   MODULEFILE
