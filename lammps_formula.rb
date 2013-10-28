e50be27b289109e33e28c050feec760bd187c82d
AnthonyDiGirolamo <anthony.digirolamo@gmail.com>
2013-10-28 14:53:15 -0400

    lammps_formula update

--- lammps_formula.rb
+++ lammps_formula.rb
@@ -1,7 +1,7 @@
 class LammpsFormula < Formula
 
   # LAMMPS 19Sep13 svn revision 10814 as per svn command below
-   
+
   homepage "http://lammps.sandia.gov/"
   url "none"
 
@@ -36,21 +36,24 @@ class LammpsFormula < Formula
 
     system "svn co svn://svn.icms.temple.edu/lammps-ro/trunk@10814 source" unless Dir.exists?("source")
 
+    host = ENV['HOSTNAME']
+    host = `hostname`
+
     Dir.chdir prefix+"/source"
     system "svn revert src/MAKE/Makefile.jaguar"
-    system "sed 's/CCFLAGS/CCFLAGS = -O2 -march=bdver1 -ftree-vectorize/' src/MAKE/Makefile.jaguar > src/MAKE/Makefile.titan"
-    system "sed -i 's/LINKFLAGS/LINKFLAGS = -O2 -march=bdver1 -ftree-vectorize/' src/MAKE/Makefile.titan"
-    system "sed -i 's/NODE_PARTITION/LAMMPS_JPEG/g' src/MAKE/Makefile.titan"
-    system "sed -i 's/JPG_LIB =/JPG_LIB = -ljpeg/' src/MAKE/Makefile.titan"
-    system "sed -i 's/CXX/CC/g' src/MAKE/Makefile.titan"
+    system "sed 's/CCFLAGS/CCFLAGS = -O2 -march=bdver1 -ftree-vectorize/' src/MAKE/Makefile.jaguar > src/MAKE/Makefile.exe"
+    system "sed -i 's/LINKFLAGS/LINKFLAGS = -O2 -march=bdver1 -ftree-vectorize/' src/MAKE/Makefile.exe"
+    system "sed -i 's/NODE_PARTITION/LAMMPS_JPEG/g' src/MAKE/Makefile.exe"
+    system "sed -i 's/JPG_LIB =/JPG_LIB = -ljpeg/' src/MAKE/Makefile.exe"
+    system "sed -i 's/CXX/CC/g' src/MAKE/Makefile.exe"
 
     Dir.chdir prefix + "/source/lib/reax"
     system "sed 's/ gfortran/ftn/g' Makefile.gfortran > Makefile.cray"
 
     Dir.chdir prefix + "/source/lib/meam"
     system "sed 's/ gfortran/ftn/g' Makefile.gfortran > Makefile.cray"
-    
-    # File.open("src/MAKE/Makefile.titan", "w+") do |file|
+
+    # File.open("src/MAKE/Makefile.exe", "w+") do |file|
     #   file.write <<-EOF.strip_heredoc
     #     test
     #   EOF
@@ -67,7 +70,7 @@ class LammpsFormula < Formula
     Dir.chdir prefix + "/source/lib/meam"
     system "make -f Makefile.cray clean"
     system "make -f Makefile.cray"
-    
+
     Dir.chdir prefix + "/source/src"
     system "make no-all clean-all"
     system "make yes-asphere yes-body yes-class2 yes-colloid yes-dipole yes-fld"
@@ -75,31 +78,33 @@ class LammpsFormula < Formula
     system "make yes-molecule yes-opt yes-reax yes-replica yes-rigid yes-shock"
     system "make yes-srd yes-user-cg-cmm yes-user-misc"
     system "make yes-gpu" if module_is_available?("cudatoolkit")
-    system "make -j8 titan"
+    system "make -j8 exe"
 
     system "mkdir -p #{prefix}/bin"
     system "cp #{prefix}/source/src/lmp_* #{prefix}/bin/"
   end
 
-  modulefile <<-EOF.strip_heredoc
-    #%Module
-    proc ModulesHelp { } {
-       puts stderr "<%= @package.name %> <%= @package.version %>"
-       puts stderr "-----------------------------------------------------------"
-       puts stderr "- Executable is lmp_titan"
-       puts stderr "- Built with asphere, body, class2, colloid, dipole, fld, "
-       puts stderr "-   granula, kspace, manybody, mc, meam, misc, molecule, "
-       puts stderr "-   opt, reax, replica, rigid, shock, srd, user-cg-cmm, "
-       puts stderr "-   user-misc packages."
-       puts stderr "- Build with gpu package on supported systems "
-       puts stderr "-----------------------------------------------------------"
-    }
-    module-whatis "<%= @package.name %> <%= @package.version %>"
+  modulefile do
+    <<-EOF.strip_heredoc
+      #%Module
+      proc ModulesHelp { } {
+         puts stderr "<%= @package.name %> <%= @package.version %>"
+         puts stderr "-----------------------------------------------------------"
+         puts stderr "- Executable is lmp_exe"
+         puts stderr "- Built with asphere, body, class2, colloid, dipole, fld, "
+         puts stderr "-   granula, kspace, manybody, mc, meam, misc, molecule, "
+         puts stderr "-   opt, reax, replica, rigid, shock, srd, user-cg-cmm, "
+         puts stderr "-   user-misc packages."
+         puts stderr "- Build with gpu package on supported systems "
+         puts stderr "-----------------------------------------------------------"
+      }
+      module-whatis "<%= @package.name %> <%= @package.version %>"
 
-    prereq PrgEnv-gnu
+      prereq #{module_is_available?("PrgEnv-gnu") ? 'PrgEnv-gnu' : 'PE-gnu'}
 
-    set PREFIX <%= @package.prefix %>
+      set PREFIX <%= @package.prefix %>
 
-    prepend-path PATH            $PREFIX/bin
-  EOF
+      prepend-path PATH            $PREFIX/bin
+    EOF
+  end
 end
