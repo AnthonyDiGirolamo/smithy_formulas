ca2e3deb4e75ad92cae4e3ecf1a6a865e1694a6b
AnthonyDiGirolamo <anthony.digirolamo@gmail.com>
2014-01-03 14:59:44 -0500

    added nco_formula

--- nco_formula.rb
+++ nco_formula.rb
@@ -1,28 +1,44 @@
 class NcoFormula < Formula
   homepage "http://nco.sourceforge.net/"
-  url "http://nco.sourceforge.net/src/nco-4.3.0.tar.gz"
-  md5 "7dc000805441cc30ff9de837e088df34"
+  url "http://nco.sourceforge.net/src/nco-4.3.9.tar.gz"
+  md5 "8f50e8b3fcec77e568d48ba452b69c47"
 
-  modules ["PrgEnv-gnu", "netcdf", "hdf5", "gsl", "java"]
+  module_commands do
+    pe = "PE-"
+    pe = "PrgEnv-" if module_is_available?("PrgEnv-gnu")
 
-  depends_on ["udunits", "gsl", "expat", "antlr2"]
+    [ "unload #{pe}gnu #{pe}pgi #{pe}cray #{pe}intel",
+      "load #{pe}gnu",
+      "load netcdf",
+      "load hdf5",
+      "load gsl",
+      "load udunits",
+      "load java" ]
+  end
+
+  depends_on ["udunits", "gsl/*/*gnu*", "expat", "antlr2"]
 
   def install
     module_list
     ENV["CC"]      = "gcc"
     ENV["CXX"]     = "g++"
 
-    ENV["CPPFLAGS"] = "-I#{expat.prefix}/include"
-    ENV["LDFLAGS"] = [
-      "-L#{expat.prefix}/lib",
-      "-Wl,-rpath,#{expat.prefix}/lib",
-      "-Wl,-rpath,/opt/gcc/4.7.0/snos/lib64",
-      "-Wl,-rpath,/opt/cray/netcdf/4.2.0/gnu/47/lib",
-      "-Wl,-rpath,#{udunits.prefix}/lib",
-      "-Wl,-rpath,#{gsl.prefix}/lib"
-    ].join(" ")
+    # ENV["CPPFLAGS"] = "-I#{expat.prefix}/include -I#{gsl.prefix}/include"
+    # ENV["LDFLAGS"] = [
+    #   "-L#{expat.prefix}/lib",
+    #   "-Wl,-rpath,#{expat.prefix}/lib",
+    #   "-Wl,-rpath,/opt/gcc/4.7.0/snos/lib64",
+    #   "-Wl,-rpath,/opt/cray/netcdf/4.2.0/gnu/47/lib",
+    #   "-Wl,-rpath,#{udunits.prefix}/lib",
+    #   "-Wl,-rpath,#{gsl.prefix}/lib"
+    # ].join(" ")
+
+    # cpp_flags = "-I#{expat.prefix}/include -I#{gsl.prefix}/include"
+    # ld_flags  = "-L#{expat.prefix}/lib -L#{gsl.prefix}/lib"
 
     system "UDUNITS2_PATH=#{udunits.prefix}",
+           # "CPPFLAGS='#{cpp_flags}'",
+           # "LDFLAGS='#{ld_flags}'",
            "HDF5_ROOT=$HDF5_DIR",
            "NETCDF4_ROOT=$NETCDF_DIR",
            "NETCDF_INC=$NETCDF_DIR/include",
@@ -33,7 +49,25 @@ class NcoFormula < Formula
            "--enable-netcdf4",
            "--disable-udunits",
            "--enable-udunits2"
+
+    system %q{for file in `grep -rl "\-L\-I\/" *` ; do sed -i 's/\-L\-I\//-I\//g' $file ; done}
     system "make"
     system "make install"
+  end
+
+  modulefile do
+    <<-MODULEFILE.strip_heredoc
+    #%Module
+    proc ModulesHelp { } {
+       puts stderr "<%= @package.name %> <%= @package.version %>"
+       puts stderr ""
+    }
+    module-whatis "<%= @package.name %> <%= @package.version %>"
+
+    set PREFIX <%= @package.prefix %>
+
+    prepend-path PATH      $PREFIX/bin
+    prepend-path MANPATH   $PREFIX/share/man
+    MODULEFILE
   end
 end
