09b4b3f6c263d6bb821706125b8142abb199652e
Suzanne T. Parete-Koon <paretekoonst@ornl.gov>
2014-11-26 13:28:11 -0500

    Latest 1.8.x version of git

--- git_formula.rb
+++ git_formula.rb
@@ -1,20 +1,20 @@
 class GitFormula < Formula
   homepage "https://git-core.googlecode.com/"
-  url "http://git-core.googlecode.com/files/git-1.8.5.1.tar.gz"
-  sha1 "dcd244c7198e8afe42ab223f7b3c9b1ae01749c3"
-  depends_on "curl"
-
-  module_commands [ "purge" ]
+  url "https://www.kernel.org/pub/software/scm/git/git-1.8.5.5.tar.gz"
+  sha1 "5b541e863389ae4fc20efd46c10334a5150cc423"
+  depends_on "curl/7.30.0"
+  module_commands [ "purge","load curl/7.30.0" ]
 
   def install
     module_list
+    puts "test"
     system "./configure --prefix=#{prefix} --with-curl=#{curl.prefix}"
     system "make"
     system "make install"
 
     system "mkdir -p #{prefix}/share/man"
-    system "curl -O https://git-core.googlecode.com/files/git-manpages-1.8.5.1.tar.gz" 
-    system "cd #{prefix}/share/man && tar xf #{prefix}/source/git-manpages-1.8.5.1.tar.gz"
+    system "wget https://git-core.googlecode.com/files/git-manpages-1.8.5.5.tar.gz" 
+    system "cd #{prefix}/share/man && tar xf #{prefix}/source/git-manpages-1.8.5.5.tar.gz"
 
     system "wget http://search.cpan.org/CPAN/authors/id/S/SH/SHLOMIF/Error-0.17021.tar.gz"
     system "tar xf Error-0.17021.tar.gz"
