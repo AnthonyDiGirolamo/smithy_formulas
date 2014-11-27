fcba51ffdceea2e5c56b4efe06abdabe6842c384
Adam Simpson <atj@ornl.gov>
2014-11-26 19:25:53 -0500

    ZeroMQ and Python bindings

---
+++ python_zmq_formula.rb
@@ -0,0 +1,65 @@
+class PythonZmqFormula < Formula
+  homepage "https://pypi.python.org/pypi/pyzmq"
+  url "https://pypi.python.org/packages/source/p/pyzmq/pyzmq-14.4.1.tar.gz#md5=a6a7fb6950b1bf5333c6795f3edf196c"
+
+  depends_on "python"
+
+  modules do
+    case build_name
+    when /python3.3/
+      [ "python/3.3.0" ]
+    when /python2.7/
+      [ "python/2.7.3" ]
+    when /python2.6/
+      [ "python/2.6.8" ]
+    end
+  end
+
+  def install
+    module_list
+
+    python_binary = "python"
+    libdirs = []
+    case build_name
+    when /python3.3/
+      python_binary = "python3.3"
+      libdirs << "#{prefix}/lib/python3.3/site-packages"
+    when /python2.7/
+      libdirs << "#{prefix}/lib/python2.7/site-packages"
+    when /python2.6/
+      libdirs << "#{prefix}/lib64/python2.6/site-packages"
+    end
+    FileUtils.mkdir_p libdirs.first
+
+    system "PYTHONPATH=$PYTHONPATH:#{libdirs.join(":")} #{python_binary} setup.py install --prefix=#{prefix} --compile"
+  end
+
+  modulefile <<-MODULEFILE.strip_heredoc
+    #%Module
+    proc ModulesHelp { } {
+       puts stderr "<%= @package.name %> <%= @package.version %>"
+       puts stderr ""
+    }
+    # One line description
+    module-whatis "<%= @package.name %> <%= @package.version %>"
+
+    if [ is-loaded python/3.3.0 ] {
+      set BUILD python3.3.0
+      set LIBDIR python3.3
+    } elseif { [ is-loaded python/2.7.3 ] || [ is-loaded python/2.7.2 ] } {
+      set BUILD python2.7.3
+      set LIBDIR python2.7
+    } else {
+      set BUILD python2.6.8
+      set LIBDIR python2.6
+    }
+    set PREFIX <%= @package.version_directory %>/$BUILD
+
+    prepend-path PATH            $PREFIX/bin
+    prepend-path LD_LIBRARY_PATH $PREFIX/lib
+    prepend-path LD_LIBRARY_PATH $PREFIX/lib64
+    prepend-path MANPATH         $PREFIX/share/man
+    prepend-path PYTHONPATH      $PREFIX/lib/$LIBDIR/site-packages
+    prepend-path PYTHONPATH      $PREFIX/lib64/$LIBDIR/site-packages
+  MODULEFILE
+end
