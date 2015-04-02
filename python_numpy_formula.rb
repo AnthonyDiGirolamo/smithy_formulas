03c4a3986ea17e95aa17fd2f145cda776c8be175
Robert.French <frenchrd@ornl.gov>
2015-04-02 11:32:05 -0400

    Update formulas for cray/lustre compatibility

--- python_numpy_formula.rb
+++ python_numpy_formula.rb
@@ -1,5 +1,6 @@
 class PythonNumpyFormula < Formula
   homepage "http://www.numpy.org/"
+  additional_software_roots [ config_value("lustre-software-root")[Smithy::Config.hostname] ]
 
   supported_build_names /python.*_gnu.*/
 
@@ -88,37 +89,39 @@ class PythonNumpyFormula < Formula
     system_python "setup.py install --prefix=#{prefix} --compile"
   end
 
-  modulefile <<-MODULEFILE.strip_heredoc
-    #%Module
-    proc ModulesHelp { } {
-       puts stderr "<%= @package.name %> <%= @package.version %>"
-       puts stderr ""
-    }
-    # One line description
-    module-whatis "<%= @package.name %> <%= @package.version %>"
-
-    prereq python
-    conflict python_numpy
-
-    <%= python_module_build_list @package, @builds %>
-    set PREFIX <%= @package.version_directory %>/$BUILD
-
-    set LUSTREPREFIX /lustre/atlas/sw/xk7/<%= @package.name %>/<%= @package.version %>/$BUILD
-
-    prepend-path LD_LIBRARY_PATH $LUSTREPREFIX/lib
-    prepend-path LD_LIBRARY_PATH $LUSTREPREFIX/lib64
-    prepend-path PYTHONPATH      $LUSTREPREFIX/lib/$LIBDIR/site-packages
-    prepend-path PYTHONPATH      $LUSTREPREFIX/lib64/$LIBDIR/site-packages
-
-    prepend-path PATH            $PREFIX/bin
-    prepend-path LD_LIBRARY_PATH $PREFIX/lib
-    prepend-path LD_LIBRARY_PATH $PREFIX/lib64
-    prepend-path LD_LIBRARY_PATH /opt/gcc/4.8.2/snos/lib64
-    prepend-path LD_LIBRARY_PATH /ccs/compilers/gcc/rhel6-x86_64/4.8.2/lib
-    prepend-path LD_LIBRARY_PATH /ccs/compilers/gcc/rhel6-x86_64/4.8.2/lib64
-    prepend-path MANPATH         $PREFIX/share/man
-
-    prepend-path PYTHONPATH      $PREFIX/lib/$LIBDIR/site-packages
-    prepend-path PYTHONPATH      $PREFIX/lib64/$LIBDIR/site-packages
-  MODULEFILE
+  modulefile do
+    <<-MODULEFILE.strip_heredoc
+      #%Module
+      proc ModulesHelp { } {
+         puts stderr "<%= @package.name %> <%= @package.version %>"
+         puts stderr ""
+      }
+      # One line description
+      module-whatis "<%= @package.name %> <%= @package.version %>"
+  
+      prereq python
+      conflict python_numpy
+  
+      <%= python_module_build_list @package, @builds %>
+      set PREFIX <%= @package.version_directory %>/$BUILD
+  
+      set LUSTREPREFIX #{additional_software_roots.first}/#{arch}/<%= @package.name %>/<%= @package.version %>/$BUILD
+  
+      prepend-path LD_LIBRARY_PATH $LUSTREPREFIX/lib
+      prepend-path LD_LIBRARY_PATH $LUSTREPREFIX/lib64
+      prepend-path PYTHONPATH      $LUSTREPREFIX/lib/$LIBDIR/site-packages
+      prepend-path PYTHONPATH      $LUSTREPREFIX/lib64/$LIBDIR/site-packages
+  
+      prepend-path PATH            $PREFIX/bin
+      prepend-path LD_LIBRARY_PATH $PREFIX/lib
+      prepend-path LD_LIBRARY_PATH $PREFIX/lib64
+      prepend-path LD_LIBRARY_PATH /opt/gcc/4.8.2/snos/lib64
+      prepend-path LD_LIBRARY_PATH /ccs/compilers/gcc/rhel6-x86_64/4.8.2/lib
+      prepend-path LD_LIBRARY_PATH /ccs/compilers/gcc/rhel6-x86_64/4.8.2/lib64
+      prepend-path MANPATH         $PREFIX/share/man
+  
+      prepend-path PYTHONPATH      $PREFIX/lib/$LIBDIR/site-packages
+      prepend-path PYTHONPATH      $PREFIX/lib64/$LIBDIR/site-packages
+    MODULEFILE
+  end
 end
