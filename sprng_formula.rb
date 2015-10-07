c36157c2f01e9e69a21e1619f2582def4e057501
Matt Belhorn <belhornmp@ornl.gov>
2015-10-07 14:17:29 -0400

    Changed fortran wrapper name mangling flags to match the compilers used at the OLCF. make.CRAY refers to older systems with a CLE compiler that upper-cases c wrapper names.

--- sprng_formula.rb
+++ sprng_formula.rb
@@ -1,4 +1,4 @@
-class Sprng2Formula < Formula
+class SprngFormula < Formula
   homepage "http://www.sprng.org/"
   url "http://www.sprng.org/Version2.0/sprng2.0b.tar.gz"
   version "2.0b"
@@ -27,21 +27,57 @@ class Sprng2Formula < Formula
   def install
     module_list
 
-    ENV["CC"]  = "mpicc"
-    ENV["CXX"] = "mpic++"
-    ENV["F77"] = "mpif77"
-    ENV["FC"]  = "mpif90"
-    ENV["F9X"] = "mpif90"
-
-    File.open("make.CHOICES", "w+") do |file|
-        file.write <<-EOF.strip_heredoc
-          PLAT = INTEL
-          MPIDEF = -DSPRNG_MPI
-          LIB_REL_DIR = lib
-        EOF
+    case Smithy::Config.arch
+    when /xk/, /xc/
+        case build_name
+        when /pgi/
+          ENV["CC"]  = "pgcc"
+          ENV["CXX"] = "pgCC"
+          ENV["F77"] = "pgf77"
+          ENV["FC"]  = "pgf90"
+          ffxn="-DAdd_"
+        when /gnu/, /gcc/
+          ENV["CC"]  = "gcc"
+          ENV["CXX"] = "g++"
+          ENV["F77"] = "gfortran"
+          ENV["FC"]  = "gfortran"
+          ffxn="-DAdd_"
+        when /intel/
+          ENV["CC"]  = "icc"
+          ENV["CXX"] = "icpc"
+          ENV["F77"] = "ifort"
+          ENV["FC"]  = "ifort"
+          ffxn="-DAdd__"
+        when /cray/
+          ENV["CC"]  = "cc"
+          ENV["CXX"] = "CC"
+          ENV["F77"] = "ftn"
+          ENV["FC"]  = "ftn"
+          ffxn="-DUpCase"
+        end
+        File.open("make.CHOICES", "w+") do |file|
+            file.write <<-EOF.strip_heredoc
+              PLAT = OLCFCRAY
+              MPIDEF = -DSPRNG_MPI
+              LIB_REL_DIR = lib
+            EOF
+        end
+    else
+        ENV["CC"]  = "mpicc"
+        ENV["CXX"] = "mpic++"
+        ENV["F77"] = "mpif77"
+        ENV["FC"]  = "mpif90"
+        ENV["F9X"] = "mpif90"
+        File.open("make.CHOICES", "w+") do |file|
+            file.write <<-EOF.strip_heredoc
+              PLAT = OLCFINTEL
+              MPIDEF = -DSPRNG_MPI
+              LIB_REL_DIR = lib
+            EOF
+        end
     end
 
-    File.open("SRC/make.INTEL", "w+") do |file|
+    File.open("SRC/make.OLCFINTEL", "w+") do |file|
         file.write <<-EOF.strip_heredoc
           AR = ar
           ARFLAGS = cr
@@ -82,9 +118,49 @@ class Sprng2Formula < Formula
         EOF
     end
 
-    system "which mpicc"
+    File.open("SRC/make.OLCFCRAY", "w+") do |file|
+        file.write <<-EOF.strip_heredoc
+          AR = ar
+          ARFLAGS = cr
+          #If your system does not have ranlib, then replace next statement with
+          #RANLIB = echo
+          RANLIB = ranlib
+          CC = cc
+          CLD = cc
+          # Set f77 to echo if you do not have a FORTRAN compiler
+          F77 = ftn
+          #F77 = echo
+          F77LD = ftn
+          FFXN = #{ffxn}
+          FSUFFIX = F
+
+          MPIF77 = ftn
+          MPICC = cc
+
+          # To use MPI, set the MPIDIR to location of mpi library, and MPILIB
+          # to name of mpi library. Remove # signs from beginning of next 3 lines.
+          # Also, if the previous compilation was without MPI, type: make realclean
+          # before compiling for mpi.
+          #
+          # COMMENTED BY ME
+          #MPIDIR = -L/usr/local/mpi/build/LINUX/ch_p4/lib
+          #MPILIB = -lmpich
+
+          # Please include mpi header file path, if needed
+
+          #CFLAGS = -O3 -DLittleEndian $(PMLCGDEF) $(MPIDEF) -D$(PLAT)  -I/usr/local/mpi/include -I/usr/local/mpi/build/LINUX/ch_p4/include
+          CFLAGS = -O3 -DLittleEndian $(PMLCGDEF) $(MPIDEF) -D$(PLAT)
+          CLDFLAGS =  -O3
+          #FFLAGS = -O3 $(PMLCGDEF) $(MPIDEF) -D$(PLAT)  -I/usr/local/mpi/include -I/usr/local/mpi/build/LINUX/ch_p4/include -I.
+          FFLAGS = -O3 $(PMLCGDEF) $(MPIDEF) -D$(PLAT)
+          F77LDFLAGS =  -O3
+
+          CPP = cpp -P
+        EOF
+    end
+
+
     # system "./configure --prefix=#{prefix} FFLAGS=-fsecond-underscore"
-    system "CC=mpicc make"
     system "make all"
 
     Dir.chdir prefix
@@ -102,6 +178,7 @@ class Sprng2Formula < Formula
 
       Usage:
         mpicc code.c $SPRNG_LIB
+        cc code.c $SPRNG_LIB
 
       The sprng module must be reloaded if you change the PrgEnv
       or you must issue a 'module update' command.
