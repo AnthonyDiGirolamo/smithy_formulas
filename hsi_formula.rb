4564406e8f999125f3cfadb8218a6d487121234c
Robert.French <frenchrd@ornl.gov>
2016-02-19 14:45:47 -0500

    Add dummy hsi formula

---
+++ hsi_formula.rb
@@ -0,0 +1,20 @@
+class HsiFormula < Formula
+  homepage "http://www.mgleicher.us/index.html/hsi/"
+
+  def install
+  end
+
+  modulefile <<-MODULEFILE.strip_heredoc
+    #%Module
+    proc ModulesHelp { } {
+       puts stderr "<%= @package.name %> <%= @package.version %>"
+       puts stderr ""
+    }
+    module-whatis "<%= @package.name %> <%= @package.version %>"
+
+    set PREFIX /sw/sources/hpss
+
+    prepend-path PATH      $PREFIX/bin
+    prepend-path MANPATH   $PREFIX/man
+  MODULEFILE
+end
