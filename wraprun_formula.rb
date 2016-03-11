c6d4f01bf5eb90c135cbd5055780090603df504c
Matt Belhorn <belhornmp@ornl.gov>
2016-03-11 15:47:42 -0500

    Updates wraprun to latest release v0.2.1 and fixes typo in YAML formula

--- wraprun_formula.rb
+++ wraprun_formula.rb
@@ -1,6 +1,12 @@
 class WraprunFormula < Formula
   homepage "https://github.com/olcf/wraprun"
-  url "https://github.com/olcf/wraprun/archive/v0.1.10.tar.gz"
+  url "https://github.com/olcf/wraprun/archive/v0.2.1.tar.gz"
+
+  supported_build_names /python2.7/, /python3/
+
+  depends_on do
+     [python_module_from_build_name, "python_yaml"]
+  end
 
   concern for_version("dev") do
     included do
@@ -10,7 +16,7 @@ class WraprunFormula < Formula
         pe = "PE-"
         pe = "PrgEnv-" if cray_system?
 
-        commands = [ "unload #{pe}gnu #{pe}pgi #{pe}cray #{pe}intel" ]
+        commands = [ "unload #{pe}gnu #{pe}pgi #{pe}cray #{pe}intel python" ]
 
         commands << "load #{pe}gnu" if build_name =~ /gnu/
         commands << "swap gcc gcc/#{$1}" if build_name =~ /gnu([\d\.]+)/
@@ -24,6 +30,10 @@ class WraprunFormula < Formula
         commands << "load #{pe}cray" if build_name =~ /cray/
         commands << "swap intel cray/#{$1}" if build_name =~ /cray([\d\.]+)/
 
+				commands << "load #{python_module_from_build_name}"
+				commands << "load python_setuptools"
+				commands << "load python_yaml"
+
         commands << "load dynamic-link"
         commands << "load cmake3"
         commands << "load git"
@@ -34,14 +44,19 @@ class WraprunFormula < Formula
       def install
         module_list
 
+				Dir.chdir "#{prefix}"
         system "rm -rf source"
         system "git clone https://github.com/olcf/wraprun.git source" unless Dir.exists?("source")
-        system "cd source; git checkout origin/development"
-        system "rm -rf build"
-        system "mkdir build"
-        system "cd build; cmake -DCMAKE_INSTALL_PREFIX=#{prefix} ../source"
-        system "cd build; make"
-        system "cd build; make install"
+				Dir.chdir "#{prefix}/source"
+        system "git checkout origin/development"
+        # normal build from here:
+        system "rm -rf build; mkdir build"
+				Dir.chdir "build"
+        system "cmake -DCMAKE_INSTALL_PREFIX=#{prefix} .."
+        system "make"
+        system "make install"
+				Dir.chdir "#{prefix}/source/python"
+				system_python "setup.py install --root=#{prefix} --prefix=''"
       end
     end
   end
@@ -50,7 +65,7 @@ class WraprunFormula < Formula
     pe = "PE-"
     pe = "PrgEnv-" if cray_system?
 
-    commands = [ "unload #{pe}gnu #{pe}pgi #{pe}cray #{pe}intel" ]
+    commands = [ "unload #{pe}gnu #{pe}pgi #{pe}cray #{pe}intel python" ]
 
     commands << "load #{pe}gnu" if build_name =~ /gnu/
     commands << "swap gcc gcc/#{$1}" if build_name =~ /gnu([\d\.]+)/
@@ -64,6 +79,10 @@ class WraprunFormula < Formula
     commands << "load #{pe}cray" if build_name =~ /cray/
     commands << "swap intel cray/#{$1}" if build_name =~ /cray([\d\.]+)/
 
+    commands << "load #{python_module_from_build_name}"
+		commands << "load python_setuptools"
+    commands << "load python_yaml"
+
     commands << "load dynamic-link"
     commands << "load cmake3"
 
@@ -72,11 +91,13 @@ class WraprunFormula < Formula
 
   def install
     module_list
-    system "rm -rf build"
-    system "mkdir build"
-    system "cd build; cmake -DCMAKE_INSTALL_PREFIX=#{prefix} .."
-    system "cd build; make"
-    system "cd build; make install"
+    system "rm -rf build; mkdir build"
+    Dir.chdir "build"
+    system "cmake -DCMAKE_INSTALL_PREFIX=#{prefix} .."
+    system "make"
+    system "make install"
+    Dir.chdir "#{prefix}/source/python"
+    system_python "setup.py install --root=#{prefix} --prefix=''"
   end
 
   modulefile <<-MODULEFILE.strip_heredoc
@@ -88,6 +109,9 @@ class WraprunFormula < Formula
     # One line description
     module-whatis "<%= @package.name %> <%= @package.version %>"
 
+    prereq python
+    module load python_yaml
+    prereq python_yaml
     module load dynamic-link
 
     setenv W_UNSET_PRELOAD 1 
@@ -96,17 +120,12 @@ class WraprunFormula < Formula
     setenv W_IGNORE_ABRT 1
     setenv W_SIG_DFL 1
 
-    <% if @builds.size > 1 %>
-    <%= module_build_list @package, @builds %>
-
+		<%= python_module_build_list @package, @builds %>
     set PREFIX <%= @package.version_directory %>/$BUILD
-    <% else %>
-    set PREFIX <%= @package.prefix %>
-    <% end %>
 
-    prepend-path PATH             $PREFIX/bin
-    prepend-path PYTHONPATH       $PREFIX/lib/python2.7/site-packages
-    prepend-path LD_LIBRARY_PATH  $PREFIX/lib
+		prepend-path PATH            $PREFIX/bin
+		prepend-path LD_LIBRARY_PATH $PREFIX/lib
+		prepend-path PYTHONPATH      $PREFIX/lib/$LIBDIR/site-packages
 
     # The libfmpich library is suffixed with the PE name, so we must extract it
     set compiler $env(PE_ENV)
